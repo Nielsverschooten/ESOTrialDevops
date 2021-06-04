55e6c62f033f161f33be1f59794ffdcde4fe3dea
Niels verschooten <niels.verschooten@student.ap.be>
2021-06-04 21:10:48 +0200

    added test proj

---
+++ ESO-trial-API/ESOTrialTests/UnitTest1.cs
@@ -0,0 +1,15 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ESO_trial_API.Models;
+
+namespace ESOTrialTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual( 1, 1 );
+        }
+    }
+}
